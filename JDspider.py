de5e1ccf19fd7f737eaef072b4bb474d6b1fe2e8
Jrz2000 <308335266@qq.com>
2021-04-27 20:12:01 +0800

    feat:Update JDspider.py.

--- JDspider.py
+++ JDspider.py
@@ -5,27 +5,28 @@ from bs4 import BeautifulSoup
 import csv
 import lxml.html
 
-def get_page(html):
+def get_page(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36'
     }
-    source = requests.get(html, headers=headers).content.decode('UTF-8')
+    source = requests.get(url, headers=headers).content.decode('UTF-8')
     #     print(source)
     selector = lxml.html.fromstring(source)
     return selector
 
 
 def spider(html):
+
     # 排名
     rank_list=[]
     for i in range(10):
         rank_list.append(i+1)
     print(rank_list)
     # 书名
-    name = html.xpath('//div[@class="p-name"]/a/em/text()').extract_first()
+    name = html.xpath('//div[@class="p-name"]/a/em')
     name_list = []
     for item in name:
-        name_list.append(item)
+        name_list.append(''.join(item.xpath('.//text()')).strip())
     print(name_list)
     # 价格
     price = html.xpath('//div[@class="p-price"]/strong/i/text()')
