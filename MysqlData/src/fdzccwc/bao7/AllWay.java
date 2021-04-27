de616733b912276c2b023d9b2985f83c95d5a67b
chenkexin <45034866+chenkexin8@users.noreply.github.com>
2021-04-27 22:50:37 +0800

    Upadte by 211804203

--- MysqlData/src/fdzccwc/bao7/AllWay.java
+++ MysqlData/src/fdzccwc/bao7/AllWay.java
@@ -23,11 +23,31 @@ public class AllWay {
     public void select(ArrayList<Books> arrayList){       //搜索数据
         this.connect();     //最后记得this.conn.close()，参考下面的look()方法
         //下方用foreach显示所有数据
-
+        for (Books book:arrayList) {
+            System.out.println("序号：" + book.getId() + "\n书名：" + book.getName() + "\n价格：" + book.getPrice() + "\n出版社：" + book.getAddress());
+        }
         //下方用sql语句查询显示搜索的数据
         int n1;
         System.out.print("输入要搜索的序号：");
         n1 = scanner.nextInt();      //n1用途：sql语句中where id = n1
+        String sql = "select * from BookData where id=" + n1;
+        try {
+            Statement stmt = null;
+            stmt = this.conn.createStatement();
+            ResultSet rs = null;
+            rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                System.out.println("序号：" + rs.getInt("id") + "\n书名：" + rs.getString("name") + "\n价格：" + rs.getDouble("price") + "\n出版社：" + rs.getString("public"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("查询失败");
+            try {
+                this.conn.close();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
     }
     public Boolean delete(ArrayList<Books> arrayList){       //删除数据
         this.connect();     //最后记得this.conn.close()，参考下面的look()方法
