ca18cb8b8fc20f80cf435db048ed8349e3cc3fe3
211806107 <45094629+211806107@users.noreply.github.com>
2021-04-27 21:08:13 +0800

    Add files via upload

---
+++ Books.java
@@ -0,0 +1,47 @@
+package fdzccwc.bao7;
+public class Books {
+    private int id;
+    private String name;
+    private Double price;
+    private String address;
+    public Books() {
+    }
+    public Books(int id, String name, Double price, String address) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
